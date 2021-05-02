496dc1598e0a9a4da3ec9fb4d10ea4b69dce1c42
Olga Kondrashova <ol.kondraschowa@gmail.com>
2021-05-02 15:57:12 +0300

    parse text to lowercase and remove punctuation

--- parse_text.py
+++ parse_text.py
@@ -1,8 +1,9 @@
 from typing import List
-from nltk import tokenize, download
+from nltk import tokenize
+import string
 
 
-def split_text(filepath: str, min_char: int = 5) -> list[str]:
+def split_text(filepath: str, min_char: int = 10) -> list[str]:
     with open(filepath, 'r', encoding='utf8') as file:
         text = file.read().replace('\n', '. ')
         text = text.replace('.”', '”.').replace('."', '".').replace('?”', '”?').replace('!”', '”!')
@@ -11,11 +12,24 @@ def split_text(filepath: str, min_char: int = 5) -> list[str]:
     sentences = tokenize.sent_tokenize(text)
     sentences = [sentence for sentence in sentences if len(sentence) >= min_char]
 
-    return list(sentences)
+    normed_text = []
+
+    for sentence in sentences:
+        sentence = sentence.lower()
+        sentence = sentence.translate(str.maketrans('', '', string.punctuation))
+        sentence = sentence.replace('“', '').replace('”', '')
+        sentence = sentence.replace('‟', '').replace('”', '')
+        sentence = sentence.replace('«', '').replace('»', '')
+        sentence = sentence.replace('—', '').replace('–', '')
+        sentence = sentence.replace('(', '').replace(')', '')
+        sentence = sentence.replace('…', '')
+        normed_text.append(sentence)
+
+    return list(normed_text)
 
 
 def main():
-    tokenized_text = split_text('/Users/olga/Documents/bachelor-degree/text.txt')
+    tokenized_text = split_text('./text.txt')
     print(tokenized_text)
 
     
