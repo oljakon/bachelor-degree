7f952b1ae76880a6cdef2ffe1679d09915c15093
Olga Kondrashova <ol.kondraschowa@gmail.com>
2021-05-08 14:55:38 +0300

    create dict corpus

--- dataset.py
+++ dataset.py
@@ -6,6 +6,7 @@ from pymorphy2 import MorphAnalyzer
 
 from parse_text import split_text, lemmatize_sentence, get_pos_sentence
 
+
 chekhov = []
 for path in glob.glob('./prose/Chekhov/*.txt'):
     chekhov += split_text(path)
@@ -30,42 +31,53 @@ turgenev = []
 for path in glob.glob('./prose/Gorky/*.txt'):
     turgenev += split_text(path)
 
-np.random.seed(1)
 
-max_len = 10000
+corpus_train = dict.fromkeys(['chekhov', 'dostoevsky', 'tolstoy', 'gogol', 'gorky', 'turgenev'])
 
-names = [chekhov, dostoevsky, tolstoy, gogol, gorky, turgenev]
+corpus_train['chekhov'] = chekhov
+corpus_train['dostoevsky'] = dostoevsky
+corpus_train['tolstoy'] = tolstoy
+corpus_train['gogol'] = gogol
+corpus_train['gorky'] = gorky
+corpus_train['turgenev'] = turgenev
 
-combined = []
-for name in names:
-    name = np.random.choice(name, max_len, replace=False)
-    combined += list(name)
 
-labels = ['Chekhov'] * max_len + ['Dostoevsky'] * max_len + ['Tolstoy'] * max_len + ['Gogol'] * max_len \
-         + ['Gorky'] * max_len + ['Turgenev'] * max_len
-
-random.seed(3)
-
-zipped = list(zip(combined, labels))
-random.shuffle(zipped)
-combined, labels = zip(*zipped)
-
-author_dataset = pd.DataFrame()
-author_dataset['text'] = combined
-author_dataset['author'] = labels
-
-author_dataset.to_csv('author_dataset.csv', index=False)
-
-morph = MorphAnalyzer()
-
-pos_text = []
-for sent in combined:
-    sent = lemmatize_sentence(sent, morph)
-    sent = get_pos_sentence(sent, morph)
-    pos_text.append(sent)
-
-author_pos_dataset = pd.DataFrame()
-author_pos_dataset['text'] = pos_text
-author_pos_dataset['author'] = labels
-
-author_pos_dataset.to_csv('author_pos_dataset.csv', index=False)
+# np.random.seed(1)
+#
+# max_len = 10000
+#
+# names = [chekhov, dostoevsky, tolstoy, gogol, gorky, turgenev]
+#
+# combined = []
+# for name in names:
+#     name = np.random.choice(name, max_len, replace=False)
+#     combined += list(name)
+#
+# labels = ['Chekhov'] * max_len + ['Dostoevsky'] * max_len + ['Tolstoy'] * max_len + ['Gogol'] * max_len \
+#          + ['Gorky'] * max_len + ['Turgenev'] * max_len
+#
+# random.seed(3)
+#
+# zipped = list(zip(combined, labels))
+# random.shuffle(zipped)
+# combined, labels = zip(*zipped)
+#
+# author_dataset = pd.DataFrame()
+# author_dataset['text'] = combined
+# author_dataset['author'] = labels
+#
+# author_dataset.to_csv('author_dataset.csv', index=False)
+#
+# morph = MorphAnalyzer()
+#
+# pos_text = []
+# for sent in combined:
+#     sent = lemmatize_sentence(sent, morph)
+#     sent = get_pos_sentence(sent, morph)
+#     pos_text.append(sent)
+#
+# author_pos_dataset = pd.DataFrame()
+# author_pos_dataset['text'] = pos_text
+# author_pos_dataset['author'] = labels
+#
+# author_pos_dataset.to_csv('author_pos_dataset.csv', index=False)
