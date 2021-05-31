f21b455e96a5b85ea78a62614a05d406f6e717a7
Olga Kondrashova <ol.kondraschowa@gmail.com>
2021-05-31 12:00:18 +0300

    add f metric

--- visualization.py
+++ visualization.py
@@ -5,34 +5,41 @@ from classification import get_train_data, svm_classification
 n = [2, 3, 4, 5, 6]
 train = []
 test = []
+f = []
 
 pos_text_train, pos_text_test, pos_author_train, pos_author_test = get_train_data('datasets/pos_2_30.csv')
-train_score2, test_score2 = svm_classification(pos_text_train, pos_text_test, pos_author_train, pos_author_test)
+train_score2, test_score2, f2 = svm_classification(pos_text_train, pos_text_test, pos_author_train, pos_author_test)
 train.append(train_score2)
 test.append(test_score2)
+f.append(f2)
 
 pos_text_train, pos_text_test, pos_author_train, pos_author_test = get_train_data('datasets/pos_3_30.csv')
-train_score3, test_score3 = svm_classification(pos_text_train, pos_text_test, pos_author_train, pos_author_test)
+train_score3, test_score3, f3 = svm_classification(pos_text_train, pos_text_test, pos_author_train, pos_author_test)
 train.append(train_score3)
 test.append(test_score3)
+f.append(f3)
 
 pos_text_train, pos_text_test, pos_author_train, pos_author_test = get_train_data('datasets/pos_4_30.csv')
-train_score4, test_score4 = svm_classification(pos_text_train, pos_text_test, pos_author_train, pos_author_test)
+train_score4, test_score4, f4 = svm_classification(pos_text_train, pos_text_test, pos_author_train, pos_author_test)
 train.append(train_score4)
 test.append(test_score4)
+f.append(f4)
 
 pos_text_train, pos_text_test, pos_author_train, pos_author_test = get_train_data('datasets/pos_5_30.csv')
-train_score5, test_score5 = svm_classification(pos_text_train, pos_text_test, pos_author_train, pos_author_test)
+train_score5, test_score5, f5 = svm_classification(pos_text_train, pos_text_test, pos_author_train, pos_author_test)
 train.append(train_score5)
 test.append(test_score5)
+f.append(f5)
 
 pos_text_train, pos_text_test, pos_author_train, pos_author_test = get_train_data('datasets/pos_6_30.csv')
-train_score6, test_score6 = svm_classification(pos_text_train, pos_text_test, pos_author_train, pos_author_test)
+train_score6, test_score6, f6 = svm_classification(pos_text_train, pos_text_test, pos_author_train, pos_author_test)
 train.append(train_score6)
 test.append(test_score6)
+f.append(f6)
 
 print(train)
 print(test)
+print(f)
 
 line1, = plt.plot(n, train,  label='Обучающая выборка')
 line2, = plt.plot(n, test, label='Тестовая выборка')
