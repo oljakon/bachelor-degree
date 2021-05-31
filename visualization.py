c91cf2961de382271a99076241379649a5760fe7
Olga Kondrashova <ol.kondraschowa@gmail.com>
2021-05-31 14:38:16 +0300

    add experiment 3

--- visualization.py
+++ visualization.py
@@ -157,8 +157,107 @@ def experiment2():
     plt.show()
 
 
+def experiment3():
+    n = [2, 3, 4, 5, 6]
+    train = []
+    test = []
+    f = []
+
+    train_verb = []
+    test_verb = []
+    f_verb = []
+
+    pos_text_train, pos_text_test, pos_author_train, pos_author_test = get_train_data('datasets/pos_2_30.csv')
+    train_score2, test_score2, f2 = svm_classification(pos_text_train, pos_text_test, pos_author_train, pos_author_test)
+    train.append(train_score2)
+    test.append(test_score2)
+    f.append(f2)
+
+    pos_text_train, pos_text_test, pos_author_train, pos_author_test = get_train_data('datasets/pos_3_30.csv')
+    train_score3, test_score3, f3 = svm_classification(pos_text_train, pos_text_test, pos_author_train, pos_author_test)
+    train.append(train_score3)
+    test.append(test_score3)
+    f.append(f3)
+
+    pos_text_train, pos_text_test, pos_author_train, pos_author_test = get_train_data('datasets/pos_4_30.csv')
+    train_score4, test_score4, f4 = svm_classification(pos_text_train, pos_text_test, pos_author_train, pos_author_test)
+    train.append(train_score4)
+    test.append(test_score4)
+    f.append(f4)
+
+    pos_text_train, pos_text_test, pos_author_train, pos_author_test = get_train_data('datasets/pos_5_30.csv')
+    train_score5, test_score5, f5 = svm_classification(pos_text_train, pos_text_test, pos_author_train, pos_author_test)
+    train.append(train_score5)
+    test.append(test_score5)
+    f.append(f5)
+
+    pos_text_train, pos_text_test, pos_author_train, pos_author_test = get_train_data('datasets/pos_6_30.csv')
+    train_score6, test_score6, f6 = svm_classification(pos_text_train, pos_text_test, pos_author_train, pos_author_test)
+    train.append(train_score6)
+    test.append(test_score6)
+    f.append(f6)
+
+    ######################################
+
+    pos_text_train, pos_text_test, pos_author_train, pos_author_test = get_train_data('datasets/pos_2_verb_30.csv')
+    train_score2, test_score2, f2 = svm_classification(pos_text_train, pos_text_test, pos_author_train, pos_author_test)
+    train_verb.append(train_score2)
+    test_verb.append(test_score2)
+    f_verb.append(f2)
+
+    pos_text_train, pos_text_test, pos_author_train, pos_author_test = get_train_data('datasets/pos_3_verb_30.csv')
+    train_score3, test_score3, f3 = svm_classification(pos_text_train, pos_text_test, pos_author_train, pos_author_test)
+    train_verb.append(train_score3)
+    test_verb.append(test_score3)
+    f_verb.append(f3)
+
+    pos_text_train, pos_text_test, pos_author_train, pos_author_test = get_train_data('datasets/pos_4_verb_30.csv')
+    train_score4, test_score4, f4 = svm_classification(pos_text_train, pos_text_test, pos_author_train, pos_author_test)
+    train_verb.append(train_score4)
+    test_verb.append(test_score4)
+    f_verb.append(f4)
+
+    pos_text_train, pos_text_test, pos_author_train, pos_author_test = get_train_data('datasets/pos_5_verb_30.csv')
+    train_score5, test_score5, f5 = svm_classification(pos_text_train, pos_text_test, pos_author_train, pos_author_test)
+    train_verb.append(train_score5)
+    test_verb.append(test_score5)
+    f_verb.append(f5)
+
+    pos_text_train, pos_text_test, pos_author_train, pos_author_test = get_train_data('datasets/pos_6_verb_30.csv')
+    train_score6, test_score6, f6 = svm_classification(pos_text_train, pos_text_test, pos_author_train, pos_author_test)
+    train_verb.append(train_score6)
+    test_verb.append(test_score6)
+    f_verb.append(f6)
+    print(train)
+    print(test)
+    print(f)
+
+    print(train_verb)
+    print(test_verb)
+    print(f_verb)
+
+    line1, = plt.plot(n, train, label='Обучающая выборка')
+    line2, = plt.plot(n, test, label='Тестовая выборка')
+    line3, = plt.plot(n, train_verb, label='Обучающая выборка (без инфинитивов)')
+    line4, = plt.plot(n, test_verb, label='Тестовая выборка (без инфинитивов)')
+    plt.xlabel('N')
+    plt.ylabel('Точность')
+    plt.legend(handles=[line1, line2, line3, line4])
+    plt.grid()
+    plt.show()
+
+    line1, = plt.plot(n, f, label='Выборка с инфинитивами')
+    line2, = plt.plot(n, f_verb, label='Выборка без инфинитивов')
+    plt.plot(n, f)
+    plt.xlabel('N')
+    plt.ylabel('F-мера')
+    plt.legend(handles=[line1, line2, line3, line4])
+    plt.grid()
+    plt.show()
+
+
 def main():
-    experiment2()
+    experiment3()
 
 
 if __name__ == '__main__':
