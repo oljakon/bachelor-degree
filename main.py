4b17970f9c3ab203687720a1b5ec4a46a3d00259
Olga Kondrashova <ol.kondraschowa@gmail.com>
2021-06-14 22:06:52 +0300

    refactor

--- main.py
+++ main.py
@@ -71,44 +71,11 @@ class MainWindow(QMainWindow):
         uni_pos_text = generate_verb_unigrams_from_n_grams(n_gram_text, morph)
         pos_n_grams_text = get_pos_n_grams_string(uni_pos_text)
 
-        n_dataset = n_table[str(n)]
-
-        pos_text_train, pos_text_test, pos_author_train, pos_author_test = get_train_data(n_dataset)
-
-        try:
-            path_array = path.split('/')
-            author_path = path_array[-2]
-            pos_text_train.append(pos_n_grams_text)
-            pos_author_train.append(author_path)
-            val = author_table[author_path]
-        except KeyError:
-            del pos_text_train[-1]
-            del pos_author_train[-1]
-
-        tfidf_vect = TfidfVectorizer()
-        x_train = tfidf_vect.fit_transform(pos_text_train)
-        x_test = tfidf_vect.transform(pos_text_test)
-
-        clf_svc = SVC(C=2.0, kernel='rbf', degree=3, gamma='scale', coef0=0.0, shrinking=True, probability=True,
-                      tol=0.001, cache_size=200, class_weight=None, verbose=False, max_iter=-1,
-                      decision_function_shape='ovo', break_ties=False, random_state=None)
-        clf_svc.fit(x_train, pos_author_train)
-
         text_test = [pos_n_grams_text]
         text_test_tfidf = tfidf_vect.transform(text_test)
         y_pred = clf_svc.predict(text_test_tfidf)
 
-        y_proba = clf_svc.predict_proba(text_test_tfidf)
-
-        proba = '(' + str(round(100 * max(y_proba[0]))) + '%)'
-
         author_pred = author_table[y_pred[0]]
-
-        # training_score = clf_svc.score(x_train, pos_author_train)
-        # test_score = clf_svc.score(x_test, pos_author_test)
-        # for doc, category in zip(pos_author_test, pred):
-        #     print('%r => %s' % (doc, category))
-        # self.author.setText(author_pred + ' ' + proba)
         self.author.setText(author_pred)
 
     @pyqtSlot(name='on_classify_clicked')
